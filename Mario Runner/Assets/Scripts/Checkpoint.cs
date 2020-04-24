0e3d86664c206bd82604aefb3cc264b6230c4a3c
Mohammad Taherian <64215843+MHTaherian@users.noreply.github.com>
2020-04-24 13:14:02 +0430

    Mario Runner Unity 2D Game

---
+++ Mario Runner/Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public LevelManager levelManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+       levelManager = FindObjectOfType<LevelManager>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+      if (other.name == "Player")
+      {
+          levelManager.currentCheckpoint= gameObject;
+          //when the player enters the second checkpoint trigger,currentCheckpoint changes
+          Debug.Log("Activated Checkpoint" + transform.position);
+      }  
+}
+}
---
+++ Mario Runner/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject currentCheckpoint;
+    private PlayerController player;
+    public GameObject deathParticle;
+    public GameObject respawnParticle;
+    public float respawnDelay;
+    // Start is called before the first frame update
+    public int pointPenaltyOnDeath;
+    private float defaultGravity;
+    private new CameraController camera;
+    public HealthSystem healthSystem;
+    void Start()
+    {
+        player= FindObjectOfType<PlayerController>();
+        
+        camera= FindObjectOfType<CameraController>();
+
+        healthSystem= FindObjectOfType<HealthSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RespawnPlayer ()
+    {
+        //move the code to RespawnPlayerCo
+        //Instantiate(deathParticle,player.transform.position,player.transform.rotation);
+        //Debug.Log("Player Respawn");
+        //player.transform.position = currentCheckpoint.transform.position;
+        //Instantiate(respawnParticle,currentCheckpoint.transform.position,currentCheckpoint.transform.rotation);
+        StartCoroutine ("RespawnPlayerCo");
+    }
+    public IEnumerator RespawnPlayerCo ()
+    {
+        Instantiate(deathParticle,player.transform.position,player.transform.rotation);
+        player.enabled= false;//stops the player actions
+        player.GetComponent<Renderer>().enabled= false;//disappears the player
+        camera.isFollowing= false;
+        //player.GetComponent<Rigidbody2D>().velocity= Vector2.zero;
+        defaultGravity=player.GetComponent<Rigidbody2D>().gravityScale;
+        player.GetComponent<Rigidbody2D>().gravityScale=0f;
+        ScoreManager.AddPoints(-pointPenaltyOnDeath);
+        Debug.Log("Player Respawn");
+        yield return new WaitForSeconds (respawnDelay);
+        player.GetComponent<Rigidbody2D>().gravityScale=defaultGravity;
+        player.transform.position = currentCheckpoint.transform.position;
+        player.enabled= true;
+        player.GetComponent<Renderer>().enabled= true;
+        camera.isFollowing=true;
+        healthSystem.FullHealth();
+        healthSystem.isDead= false;
+        Instantiate(respawnParticle,currentCheckpoint.transform.position,currentCheckpoint.transform.rotation);
+    }
+}
