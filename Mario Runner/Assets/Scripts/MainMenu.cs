15d603857fa318d14f7d8b975ba965f54339bdc0
Mohammad Taherian <64215843+MHTaherian@users.noreply.github.com>
2020-05-09 13:06:41 +0430

    Mario Runner

---
+++ Mario Runner/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string startLevel;
+    public string levelSelect;
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(startLevel);
+    }
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene(levelSelect);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
